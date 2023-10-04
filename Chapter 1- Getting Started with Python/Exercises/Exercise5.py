fc857b85c47bbdfeb9a7a9f932df85ae040add8b
John Richard <johnrichard20506@gmail.com>
2023-10-04 11:18:00 +0400

    Revert "Revert "ex5""
    This reverts commit 1cd0c149294571d8831112f39f3a0e8820f7b672.

---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise5.py
@@ -0,0 +1,5 @@
+import math
+R = float(input("Enter the radius: "))
+pi = math.pi
+area = pi * (R*2)
+print(area)
---
+++ Chapter 1- Getting Started with Python/practice4.py
@@ -0,0 +1,6 @@
+B = int(input("Enter the base number: "))
+H = int(input("Enter the height number: "))
+
+area = 0.5 * B * H
+
+print("The area of the triangle is:", area)
