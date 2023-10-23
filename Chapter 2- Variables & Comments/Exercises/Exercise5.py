429d8bea273c029596c93f72aad3f06673c1747e
John Richard <johnrichard20506@gmail.com>
2023-10-23 11:38:16 +0400

    chapter 2

--- Chapter 2- Variables & Comments/Exercises/Exercise5.py
+++ Chapter 2- Variables & Comments/Exercises/Exercise5.py
@@ -9,7 +9,7 @@ usb = cash//price
 
 print("The girl can buy", usb, "pounds worth of usb sticks")
 
-#amount of change left from buying USB sticks
+# amount of change left from buying USB sticks
 # calculate amount used + change
 
 used_money = usb * price
