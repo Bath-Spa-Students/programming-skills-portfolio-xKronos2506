bac4868886a59e396a93e4e9ea261eba7a57b317
John Richard <johnrichard20506@gmail.com>
2023-10-23 11:30:16 +0400

    Chapter 2 Exercise 5

--- Chapter 2- Variables & Comments/Exercises/Exercise5.py
+++ Chapter 2- Variables & Comments/Exercises/Exercise5.py
@@ -5,15 +5,15 @@ price = 6
 
 # amount of usb sticks that can be bought
 
-amnt = cash//price
+usb = cash//price
 
-print("The girl can buy",amnt, "pieces worth of usb sticks")
+print("The girl can buy", usb, "pounds worth of usb sticks")
 
 #amount of change left from buying USB sticks
 # calculate amount used + change
 
-used_money = amnt * price
+used_money = usb * price
 change = cash - used_money
 
-print(used_money)
-print("The girl has change worth",change ,"pounds")
+print("THe girl used ", used_money, "pounds")
+print("The girl has change worth", change ,"pounds")
