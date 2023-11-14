b26b39c7a6b011f0802f7de9080ad4be0805e4f3
John Richard <johnrichard20506@gmail.com>
2023-11-14 21:25:34 +0400

    dICTIONARIES

--- Chapter 5- Dictionaries/Ex4.py
+++ Chapter 5- Dictionaries/Ex4.py
@@ -7,12 +7,16 @@ major_rivers = {"Nile" : "Egypt",
 for river, country in major_rivers.items():
     print(f"\nThe {river} runs through {country}")
 
+#River title
 print("\nRivers:")
 
+#for loop for the key in major_rivers 
 for river in major_rivers.keys():
     print("\t", river)
 
+#countries title
 print("\nCountries:")
 
+#for loop for the value in major_rivers
 for country in major_rivers.values():
     print("\t", country)
--- Chapter 5- Dictionaries/Ex5.py
+++ Chapter 5- Dictionaries/Ex5.py
@@ -4,8 +4,11 @@ pets_dicts_2 = {"Pet": "British Shorthair Cat", "Owner Name": "Yunjin"}
 pets_dicts_3 = {"Pet": "Cockatiel", "Owner Name": "Hanni"}
 pets_dicts_4 = {"Pet": "Chinese Striped Hamster", "Owner Name": "Danielle"}
 
+#all dictionaries concatenated into a list called pet_list
 pets_lists = [pets_dicts_1, pets_dicts_2, pets_dicts_3, pets_dicts_4]
 
+#for loop from the pet_list 
 for pets in pets_lists:
+#print statement for both pet name and owner name within the pet_list variable.
     print(f"Pet Breed and Animal Name: {pets['Pet']}")
     print(f"Owner Name: {pets['Owner Name']}\n")
