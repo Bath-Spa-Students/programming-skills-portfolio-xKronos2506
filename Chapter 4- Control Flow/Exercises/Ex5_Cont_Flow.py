56f48e927e8efefafc9f9d5b16a8f55aab83fd76
John Richard <johnrichard20506@gmail.com>
2023-11-18 20:55:35 +0400

    Update Ex5_Cont_Flow.py

--- Chapter 4- Control Flow/Exercises/Ex5_Cont_Flow.py
+++ Chapter 4- Control Flow/Exercises/Ex5_Cont_Flow.py
@@ -3,8 +3,8 @@ favorite_fruits = ['mango', 'apple', 'avocado']
 if 'mango' in favorite_fruits:
     print("You like mangos")
 if 'apple' in favorite_fruits:
-    print("ambobo mo")
+    print("You like apples")
 if 'strawberry' in favorite_fruits:
-    print("rhianne favorite")
+    print("You like strawberries")
 if 'avocado' in favorite_fruits:
-    print("hello broskie")
+    print("You like avocados")
